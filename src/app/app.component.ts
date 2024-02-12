eaf9ed02876b9af0038bc41fe486e654a9338061
Divyesh0208 <divyeshundhad16@gmail.com>
2024-02-13 00:53:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+  todoList:string[]=[];
+  getData:any;
+  showBox = false;
+
+  constructor(){
+    this.addToStorage();
+  }
+
+  add(todo:string){
+    console.log(todo);
+    this.todoList.push(todo);
+    console.log("this.todoList: ", this.todoList);
+    localStorage.setItem("toDo",JSON.stringify(this.todoList));
+    this.addToStorage();
+  }
+  addToStorage(){
+    if(localStorage.getItem("toDo")){
+      this.showBox=true;
+      this.getData =localStorage.getItem("toDo");
+      this.getData = JSON.parse(this.getData);
+      this.todoList = this.getData;
+      console.log("this.getData : ",this.getData );
+    }
+  }
+  delete(index:number){
+    console.log("index: ", index);
+    this.removeItem(index);
+  }
+  removeItem(index:number){
+    let updatedData = this.getData.splice(index,1);
+    updatedData = this.getData.filter((word:any) => word !== updatedData);
+    localStorage.setItem("toDo",JSON.stringify(updatedData));
+    console.log("updatedData : ", updatedData );
+  }
+  edit(value:string,id:number){
+    this.getData[id]=value;
+    localStorage.setItem("toDo",JSON.stringify(this.getData));
+  }
+
 }
